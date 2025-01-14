6b635bc2a289e7d77b273deb6077b4f639b4da19
Mahantesh Patil <mahantesh.patil67@gmail.com>
2025-01-14 09:58:30 +0530

    products link change

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,7 +1,7 @@
 import ContactUsSection from "../components/contact-us";
 import FooterSection from "../components/footer";
 import Navbar from "../components/nav-bar";
-import { CarePassTable, ProductsIntro } from '../components/extras';
+import { CarePassTable, ProductsIntro, ProductsIntroTwo } from '../components/extras';
 import TrustedPartnersSection from "../components/trusted-partners";
 
 const Products = () => {
@@ -11,6 +11,7 @@ const Products = () => {
             <ProductsIntro />
             <CarePassTable />
             <TrustedPartnersSection />
+            {/* <ProductsIntroTwo /> */}
             <ContactUsSection />
             <FooterSection />
         </>
